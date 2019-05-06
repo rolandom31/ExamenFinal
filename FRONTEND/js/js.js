d358449e0b674544ec5ec211cd08402ae9877cb6
Rolando Mata <rdma95@gmail.com>
2019-05-06 17:59:21 -0500

    delete

--- FRONTEND/js/js.js
+++
@@ -1,7 +0,0 @@
-$('.input_wrap').on('click', function(event){
-  $(this).addClass('input_wrap_sel');
-});
-
-$('.input_wrap').focusout(function(event){
-  $(this).removeClass('input_wrap_sel');
-});
